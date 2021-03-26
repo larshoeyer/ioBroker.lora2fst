48378f192ff6e2bfb7636c7213de8b2349348ad7
larshoeyer <lars.hoeyer@fst.com>
2021-03-26 13:35:43 +0100

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'lora2fst adapter settings': {
+        'en': 'Adapter settings for lora2fst',
+        'de': 'Adaptereinstellungen für lora2fst',
+        'ru': 'Настройки адаптера для lora2fst',
+        'pt': 'Configurações do adaptador para lora2fst',
+        'nl': 'Adapterinstellingen voor lora2fst',
+        'fr': "Paramètres d'adaptateur pour lora2fst",
+        'it': "Impostazioni dell'adattatore per lora2fst",
+        'es': 'Ajustes del adaptador para lora2fst',
+        'pl': 'Ustawienia adaptera dla lora2fst',
+        'zh-cn': 'lora2fst的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
